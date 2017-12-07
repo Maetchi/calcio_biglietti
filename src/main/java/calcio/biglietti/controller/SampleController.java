c7a48fbe17fccc9ed7ff6de87838be25a878b6a6
maeda <io.giuoco.a.calcio@gmail.com>
2017-12-07 21:02:10 +0900

    初期設定

---
+++ src/main/java/calcio/biglietti/controller/SampleController.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package calcio.biglietti.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author maeda
+ *
+ */
+@Controller
+public class SampleController {
+
+	@RequestMapping(value = "/", method = GET)
+	public String show() {
+		// jspのファイル名
+		return "test";
+	}
+}
